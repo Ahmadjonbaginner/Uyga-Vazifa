640f8b04245d02aa51e663cfb7f9f0a780ec01b9
Ahmadjonbeginner <ahmadjonu333@gmail.com>
2023-02-24 02:41:06 +0500

    27-dars bajarildi

--- js/index.js
+++ js/index.js
@@ -1,1 +1,73 @@
 //js
+// dars vaqtidagi amaliyot 1.1
+// const country = 'Uzbekiston';
+// let population = 30000000;
+// const continent = 'Osyo'
+// console.log(country);
+// console.log(population);
+// console.log(continent)
+
+// 1.2
+// const userName = +prompt('yoshingiz')
+// if (userName >= 18) {
+//     console.log('berilsin');
+// } else {
+//     console.log('berilmasin');
+// }
+
+// 1.3
+// const yourJob = prompt('sizning kasbingiz')
+// if (yourJob === 'student') {
+//     console.log('yaxshilab oqishingiz kerak');
+// } else if (yourJob === 'teachr') {
+//     console.log('sabr');
+// } else {
+//     console.log('ishlashingiz kerak');
+// }
+
+//1.4
+// const number = +prompt('juft son kiriting');
+// if (number % 2 === 0) {
+//     console.log('juft');
+// } else {
+//     console.log('toq');
+// }
+
+// 1.5
+// const num1 = +prompt('birinchi sonni kiriting');
+// const num2 = +prompt('ikkinchi sonni kiriting');
+// if (num1 > num2) {
+//     console.log(`birinchi son kata ${num1}`);
+// } else if (num1 < num2) {
+//     console.log(`ikkinchisi katta ${num2}`);
+// } else {
+//     console.log(`ikkisi ham teng ${num1} = ${num2}`);
+// }
+
+// Uyga Vazifa 1.1
+// const your_of_birth = +prompt('Tug`ulgan yilingizni kiriting');
+// const year = 2000
+// if (your_of_birth >= year) {
+//     console.log('Siz 21-asrda tug`ulgansiz');
+// } else {
+//     console.log('Siz 20-asirda tug`ulgansiz');
+// }
+
+// 1.2
+// const num1 = +prompt('birinchi soni kiriting');
+// const num2 = +prompt('ikkinchi sonni kiriting');
+// const num3 = +prompt('javobini yozing');
+// if (num1 + num2 === num3) {
+//     console.log(`sizning javobingiz togri ${num3}`);
+// } else {
+//     console.log(`sizning javobingiz notogri togri javob ${num1 + num2}`);
+// }
+
+// 1.3
+// const amount = +prompt('Iltimos sizda qancha miqdorda mulingiz borlihini kiriting somda');
+// let amount1 = 9690000;
+// if (amount >= amount1) {
+//     console.log(`Oq yo’l, Alisher!`);
+// } else {
+//     console.log(`Alisher, ozgina sabr qilish kerak bo’lar ekan.Sizga yana ${amount1-amount} suncha miqdor yetmayapti.`);
+// }
