a84cb5b84c72c36f6b34c91cb3b38d81e6754c4c
Ahmadjonbeginner <ahmadjonu333@gmail.com>
2023-02-27 17:52:03 +0500

    28-dars amaliyot va uyga vazifa bajarildi

--- js/index.js
+++ js/index.js
@@ -71,3 +71,96 @@
 // } else {
 //     console.log(`Alisher, ozgina sabr qilish kerak bo’lar ekan.Sizga yana ${amount1-amount} suncha miqdor yetmayapti.`);
 // }
+
+// Dars vatidagi amaliyot 1.1
+
+// const yourEye = 0;
+// const yourDocument = true;
+// if (yourEye || )
+
+// 1.2
+// const massJohn = +prompt('John vazningizni kiriting');
+// const heightJohn = +prompt('John boyingizni kiriting');
+// const massDog = +prompt('Dog vazningizni kiriting');
+// const heightDog = +prompt('Dog boyingizni kiriting');
+// let allJohn = (`${massJohn / heightJohn ** 2}`);
+// let allDog = (`${massDog / heightDog ** 2}`);
+// if (allDog > allJohn) {
+//     console.log(`Dog sizniki katta ${allDog}`);
+// } else if (allDog === allJohn) {
+//     console.log(`ikkingizniki ham teng ${allDog} = ${allJohn}`);
+// } else {
+//     console.log(`John sizniki katta ${allJohn}`);
+// }
+
+// 1.3
+// const num1 = +prompt('son kiriting');
+// if (num1 > 0) {
+//     console.log(`${num1+10}`);
+// } else {
+//     console.log(num1);
+// }
+
+// 1.4
+// const num1 = +prompt('son kiriting');
+// if (num1 > 0) {
+//     console.log(`${num1+10}`);
+// } else {
+//     console.log(num1 + 2);
+// }
+
+// 1.5
+// const num1 = +prompt('son kiriting');
+// let all = (`${num1 === 0}`);
+// if (num1 > 0) {
+//     console.log(`${num1+10}`);
+//     if (num1 < 0) {
+//         console.log(num1 + 2)
+//     }
+// } else {
+//     console.log(all + 10);
+// }
+
+// 1.6
+// const num1 = +prompt('birinchi sonni kiriting');
+// const num2 = +prompt('ikkinchi sonni kiriting');
+// const num3 = +prompt('uchunchi sonni kiriting');
+
+// if6
+// const num1 = +prompt('Birinchi sonni kiriting');
+// const num2 = +prompt('Ikkinchi sonni kiriting');
+// if (num1 > num2) {
+//     console.log(`Birinchi son katta ${num1}`);
+// } else if (num1 === num2) {
+//     console.log(`Ikkisi ham teng ${num1}=${num2}`);
+// } else {
+//     console.log(`IKkinchi son katta ${num2}`);
+// }
+
+// if7
+
+// Uyga vazifa 1.1
+// const massJohn = +prompt('John vazningizni kiriting');
+// const heightJohn = +prompt('John boyingizni kiriting');
+// const massDog = +prompt('Dog vazningizni kiriting');
+// const heightDog = +prompt('Dog boyingizni kiriting');
+// let allJohn = (`${massJohn / heightJohn ** 2}`);
+// let allDog = (`${massDog / heightDog ** 2}`);
+// if (allDog > allJohn) {
+//     console.log(`Dog sizniki katta ${allDog}`);
+// } else if (allDog === allJohn) {
+//     console.log(`ikkingizniki ham teng ${allDog} = ${allJohn}`);
+// } else {
+//     console.log(`John sizniki katta ${allJohn}`);
+// }
+
+// 1.2
+// const userAge = prompt('Iltimos yoshingizni kiriting.');
+// let numAge = Number(userAge);
+// if (numAge <= 18) {
+//     console.log(`Siz oqishingiz kerak`);
+// } else if (numAge <= 60) {
+//     console.log(`Siz mehnat qilishingiz kerak`);
+// } else {
+//     console.log(`Siz pensiya yoshidasiz pensiyani olib nevaralarizga ham berib turing`)
+// }
